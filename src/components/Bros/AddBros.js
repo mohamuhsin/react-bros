5679db35f6acacf97d1518c488cd08f377d7f022
mohamuhsin <muhsinohoho@outlook.com>
2023-06-14 09:56:31 +0300

    components done

--- src/components/Bros/AddBros.js
+++ src/components/Bros/AddBros.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import classes from "./AddBros.module.css";
 
 const AddBros = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
   return (
-    <Card>
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="name">Name</label>
         <input id="name" type="text" />
--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,8 +1,14 @@
 import React from "react";
 
+import classes from "./Button.module.css";
+
 const Button = (props) => {
   return (
-    <button type={props.type || "button"} onClick={props.onClick}>
+    <button
+      className={classes.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
   );
