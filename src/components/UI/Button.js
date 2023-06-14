9e572c2b25dcb37a29b45df8fd44ec754dd7cdf1
mohamuhsin <muhsinohoho@outlook.com>
2023-06-14 09:51:15 +0300

    built other components

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Button = (props) => {
-  return <button>{props.type || "button"}</button>;
+  return (
+    <button type={props.type || "button"} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
